78c5006b2abb29ed5978a1affd42444b039d275e
saswatbehera14 <74148575+saswatbehera14@users.noreply.github.com>
2024-06-14 16:49:48 +0530

    header

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React code with buddy Simplicitylabs email alert checking
-        </a>
-      </header>
+     <h1>welcome to buddy </h1>
+     <h2>cicd pipe line is running</h2>
     </div>
   );
 }
