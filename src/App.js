f901ac36c80dddf56a49ebb66b4ccaec68763bd3
saswatbehera14 <74148575+saswatbehera14@users.noreply.github.com>
2024-06-14 16:42:09 +0530

    cicd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React code with buddy Simplicitylabs email alert in cicd
+          Simplicitylabs email alert in cicd
         </a>
       </header>
     </div>
